c8a70c9bdc0f4a57be10418567ab50e59057e4b8
somejeff <somejeff@gmail.com>
2019-05-10 23:09:48 -0400

    Why is this not checked in?

---
+++ src/views/Generator.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <Generator/>
+  </v-container>
+</template>
+
+<script>
+import Generator from "../components/Generator";
+
+export default {
+  components: {
+    Generator
+  }
+};
+</script>
